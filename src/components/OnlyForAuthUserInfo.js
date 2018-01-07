73a0af602bb57c6d39c1dcc1cf6497a8860c82a9
Pavel Golman <pavel.golman@postindustria.com>
2018-01-08 00:27:17 +0200

    Fixed info only for auth users

--- src/components/OnlyForAuthUserInfo.js
+++ src/components/OnlyForAuthUserInfo.js
@@ -3,20 +3,27 @@ import { Link, withRouter } from 'react-router-dom';
 
 class OnlyForAuthUserInfo extends Component {
   componentWillMount() {
-    console.log("componentWillMount OnlyForAuthUserInfo");
+    if(this.props.isAuthenticated){
+      this.props.onlyForAuthUserInfoAction();
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(!this.props.isAuthenticated && nextProps.isAuthenticated){
+      this.props.onlyForAuthUserInfoAction();
+    }
   }
 
   info(){
-
-    return (
-      <span>sdfsfsdf</span>
-    )
+    if(!this.props.loading && this.props.data){
+      return this.props.data.map((item, i) => <li key={i}>{item.title}</li>)
+    }
+    return <p>INFO ONLY FOR AUTH USERS</p>
   }
 
   render() {
     return (
       <div>
-        INFO ONLY FOR AUTH USERS<br />
         {this.info()}
       </div>
     );
